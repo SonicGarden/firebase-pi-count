e98bef5b0ddb3e37722da101b3b22504c035035e
t-kojima <t-kojima@sonicgarden.jp>
2023-07-25 14:54:46 +0900

    feat: functionsへの登録は呼び出し元で行うように

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as functions from 'firebase-functions';
 import axios from 'axios';
 
 const KINTONE_URL = 'https://sonicgarden.cybozu.com/k/v1/record.json';
@@ -13,49 +12,32 @@ type Data = {
   paymentSystemName?: string;
 };
 
-export const piCount = ({
-  region = 'asia-northeast1',
-  schedule = '0 0 1 * *',
-  timeZone = 'Asia/Tokyo',
+export const piCount = async ({
   kintoneApiToken,
   data: func,
 }: {
-  region?: string;
-  schedule?: string;
-  timeZone?: string;
   kintoneApiToken: string;
   data: () => Promise<Data>;
-}): functions.CloudFunction<unknown> => {
-  return functions
-    .region(region)
-    .pubsub.schedule(schedule)
-    .timeZone(timeZone)
-    .onRun(async () => {
-      try {
-        const data = await func();
-        await axios.post(
-          KINTONE_URL,
-          {
-            app: KINTONE_APP_ID,
-            record: {
-              type: { value: data.type },
-              platform: { value: data.platform },
-              name: { value: data.name },
-              count: { value: data.count },
-              is_important_private_info: { value: data.isImportantPrivateInfo ? 'true' : 'false' },
-              payment_system_name: { value: data.paymentSystemName },
-            },
-          },
-          {
-            headers: {
-              'X-Cybozu-API-Token': kintoneApiToken,
-              'Content-Type': 'application/json',
-            },
-          }
-        );
-        console.info('send pi count, success');
-      } catch (err) {
-        console.error(err);
-      }
-    });
+}) => {
+  const data = await func();
+  return axios.post(
+    KINTONE_URL,
+    {
+      app: KINTONE_APP_ID,
+      record: {
+        type: { value: data.type },
+        platform: { value: data.platform },
+        name: { value: data.name },
+        count: { value: data.count },
+        is_important_private_info: { value: data.isImportantPrivateInfo ? 'true' : 'false' },
+        payment_system_name: { value: data.paymentSystemName },
+      },
+    },
+    {
+      headers: {
+        'X-Cybozu-API-Token': kintoneApiToken,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
 };
